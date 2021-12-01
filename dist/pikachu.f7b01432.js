2e83ba20bb7b84134a15229911c44b422cd2b7d0
ElvinsYan <yanxiangyang94@gmail.com>
2021-12-01 21:46:36 +0800

    update

--- dist/pikachu.f7b01432.js
+++ dist/pikachu.f7b01432.js
@@ -103,7 +103,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   // Override the current require with this new one
   return newRequire;
-})({"css.js":[function(require,module,exports) {
+})({"K4Xi":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -112,7 +112,7 @@ Object.defineProperty(exports, "__esModule", {
 var string = "\n.skin * {\n  box-sizing: border-box;\n  margin: 0;\n  padding: 0;\n}\n\n.skin *::before,\n.skin *::after {\n  box-sizing: border-box;\n}\n\n.skin {\n  background: #ffe600;\n  min-height: 50vh;\n  position: relative;\n}\n\n.nose {\n  border-top: 8px solid #000000;\n  border-right: 10px solid transparent;\n  border-left: 10px solid transparent;\n  position: absolute;\n  top: 200px;\n  left: 50%;\n  margin-left: -10px;\n}\n\n.nose .arc {\n  position: absolute;\n  background-color: #000000;\n  height: 5px;\n  width: 20px;\n  top: -13px;\n  left: -10px;\n  border-top-left-radius: 10px 5px;\n  border-top-right-radius: 10px 5px;\n}\n\n@keyframes shake {\n  0% {\n    transform: rotate(0deg);\n  }\n  33% {\n    transform: rotate(5deg);\n  }\n  67% {\n    transform: rotate(-5deg);\n  }\n  100% {\n    transform: rotate(0deg);\n  }\n}\n\n.nose:hover {\n  animation: shake 250ms infinite linear;\n}\n\n.eye {\n  border: 3px solid #000000;\n  border-radius: 50%;\n  height: 64px;\n  width: 64px;\n  position: absolute;\n  top: 160px;\n  left: 50%;\n  margin-left: -32px;\n  background: #2e2e2e;\n}\n\n.eye::after {\n  content: \"\";\n  display: block;\n  border: 3px solid #000000;\n  width: 32px;\n  height: 32px;\n  background: #ffffff;\n  border-radius: 50%;\n  position: absolute;\n  left: 6px;\n  top: -1px;\n}\n\n.eye.left {\n  transform: translateX(-120px);\n}\n\n.eye.right {\n  transform: translateX(120px);\n}\n\n.mouth {\n  position: absolute;\n  width: 200px;\n  height: 200px;\n  left: 50%;\n  margin-left: -100px;\n  top: 230px;\n  overflow: hidden;\n}\n\n.mouth .up {\n  position: relative;\n  top: -3px;\n  z-index: 1;\n}\n\n.mouth .up .lip {\n  border: 3px solid #000000;\n  border-top: none;\n  width: 88px;\n  height: 25px;\n  position: absolute;\n  top: 0;\n  left: auto;\n  background: #ffe600;\n  box-shadow: 0 -10px 0 #ffe600;\n}\n\n.mouth .up .lip.left {\n  border-right: none;\n  border-bottom-left-radius: 44px 25px;\n  transform: rotate(-25deg);\n  right: 50%;\n}\n\n.mouth .up .lip.right {\n  border-left: none;\n  border-bottom-right-radius: 44px 25px;\n  transform: rotate(25deg);\n  left: 50%;\n}\n\n.mouth .down {\n  width: 180px;\n  height: 800px;\n  border: 4px solid #000000;\n  position: absolute;\n  top: -630px;\n  left: 50%;\n  margin-left: -90px;\n  border-radius: 50% 50%;\n  background: #9b000a;\n  overflow: hidden;\n}\n\n.mouth .down .tongue {\n  width: 180px;\n  height: 200px;\n  position: absolute;\n  bottom: -64px;\n  left: 50%;\n  margin-left: -90px;\n  border-radius: 50%;\n  background: #ff485f;\n}\n\n.face {\n  position: absolute;\n  border: 3px solid black;\n  width: 88px;\n  height: 88px;\n  border-radius: 50%;\n  left: 50%;\n  top: 270px;\n  margin-left: -44px;\n  background: #ff0000;\n}\n\n.face.left {\n  transform: translateX(-166px);\n}\n\n.face.right {\n  transform: translateX(166px);\n}\n";
 
 exports.default = string;
-},{}],"pikachu.js":[function(require,module,exports) {
+},{}],"3THh":[function(require,module,exports) {
 'use strict';
 
 var _css = require('./css.js');
@@ -184,175 +184,5 @@ var player = {
 };
 
 player.init();
-},{"./css.js":"css.js"}],"C:\\Users\\yxy19\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
-var global = arguments[3];
-var OVERLAY_ID = '__parcel__error__overlay__';
-
-var OldModule = module.bundle.Module;
-
-function Module(moduleName) {
-  OldModule.call(this, moduleName);
-  this.hot = {
-    data: module.bundle.hotData,
-    _acceptCallbacks: [],
-    _disposeCallbacks: [],
-    accept: function (fn) {
-      this._acceptCallbacks.push(fn || function () {});
-    },
-    dispose: function (fn) {
-      this._disposeCallbacks.push(fn);
-    }
-  };
-
-  module.bundle.hotData = null;
-}
-
-module.bundle.Module = Module;
-
-var parent = module.bundle.parent;
-if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
-  var hostname = '' || location.hostname;
-  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '5410' + '/');
-  ws.onmessage = function (event) {
-    var data = JSON.parse(event.data);
-
-    if (data.type === 'update') {
-      console.clear();
-
-      data.assets.forEach(function (asset) {
-        hmrApply(global.parcelRequire, asset);
-      });
-
-      data.assets.forEach(function (asset) {
-        if (!asset.isNew) {
-          hmrAccept(global.parcelRequire, asset.id);
-        }
-      });
-    }
-
-    if (data.type === 'reload') {
-      ws.close();
-      ws.onclose = function () {
-        location.reload();
-      };
-    }
-
-    if (data.type === 'error-resolved') {
-      console.log('[parcel] ✨ Error resolved');
-
-      removeErrorOverlay();
-    }
-
-    if (data.type === 'error') {
-      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
-
-      removeErrorOverlay();
-
-      var overlay = createErrorOverlay(data);
-      document.body.appendChild(overlay);
-    }
-  };
-}
-
-function removeErrorOverlay() {
-  var overlay = document.getElementById(OVERLAY_ID);
-  if (overlay) {
-    overlay.remove();
-  }
-}
-
-function createErrorOverlay(data) {
-  var overlay = document.createElement('div');
-  overlay.id = OVERLAY_ID;
-
-  // html encode message and stack trace
-  var message = document.createElement('div');
-  var stackTrace = document.createElement('pre');
-  message.innerText = data.error.message;
-  stackTrace.innerText = data.error.stack;
-
-  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
-
-  return overlay;
-}
-
-function getParents(bundle, id) {
-  var modules = bundle.modules;
-  if (!modules) {
-    return [];
-  }
-
-  var parents = [];
-  var k, d, dep;
-
-  for (k in modules) {
-    for (d in modules[k][1]) {
-      dep = modules[k][1][d];
-      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
-        parents.push(k);
-      }
-    }
-  }
-
-  if (bundle.parent) {
-    parents = parents.concat(getParents(bundle.parent, id));
-  }
-
-  return parents;
-}
-
-function hmrApply(bundle, asset) {
-  var modules = bundle.modules;
-  if (!modules) {
-    return;
-  }
-
-  if (modules[asset.id] || !bundle.parent) {
-    var fn = new Function('require', 'module', 'exports', asset.generated.js);
-    asset.isNew = !modules[asset.id];
-    modules[asset.id] = [fn, asset.deps];
-  } else if (bundle.parent) {
-    hmrApply(bundle.parent, asset);
-  }
-}
-
-function hmrAccept(bundle, id) {
-  var modules = bundle.modules;
-  if (!modules) {
-    return;
-  }
-
-  if (!modules[id] && bundle.parent) {
-    return hmrAccept(bundle.parent, id);
-  }
-
-  var cached = bundle.cache[id];
-  bundle.hotData = {};
-  if (cached) {
-    cached.hot.data = bundle.hotData;
-  }
-
-  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
-    cached.hot._disposeCallbacks.forEach(function (cb) {
-      cb(bundle.hotData);
-    });
-  }
-
-  delete bundle.cache[id];
-  bundle(id);
-
-  cached = bundle.cache[id];
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    cached.hot._acceptCallbacks.forEach(function (cb) {
-      cb();
-    });
-    return true;
-  }
-
-  return getParents(global.parcelRequire, id).some(function (id) {
-    return hmrAccept(global.parcelRequire, id);
-  });
-}
-},{}]},{},["C:\\Users\\yxy19\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js","pikachu.js"], null)
-//# sourceMappingURL=/pikachu.f7462dfc.map
+},{"./css.js":"K4Xi"}]},{},["3THh"], null)
+//# sourceMappingURL=pikachu.f7b01432.map
